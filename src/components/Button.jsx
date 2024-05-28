373f297bf50a1652412f54441c65d13aff1ef3d5
Youssef <speedyoussef@gmail.com>
2024-05-28 14:11:10 +0100

    added Button component of notes

---
+++ src/components/Button.jsx
@@ -0,0 +1,50 @@
+function Button({ onClick, children }) {
+    return (
+      <button onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+  
+  function PlayButton({ movieName }) {
+    function handlePlayClick() {
+      alert(`Playing ${movieName}!`);
+    }
+  
+    return (
+      <Button onClick={handlePlayClick}>
+        Play "{movieName}"
+      </Button>
+    );
+  }
+  
+  function UploadButton() {
+    return (
+      <Button onClick={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
+    );
+  }
+  
+  export default function Toolbar() {
+    return (
+      <div>
+        <PlayButton movieName="Kiki's Delivery Service" />
+        <UploadButton />
+      </div>
+    );
+  }
+  
+  export default function ColorSwitch({
+    onChangeColor
+  }) {
+    return (
+      <button onClick={e => {
+        e.stopPropagation();
+        onChangeColor();
+      }}>
+        Change color
+      </button>
+    );
+  }
+  
